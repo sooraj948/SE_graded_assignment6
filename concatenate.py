26defc1c87ea4efad166b5727a5b6734cf15d96f
Sooraj Sathish <sooraj@Soorajs-MacBook-Air.local>
2022-11-24 18:44:03 +0530

    Created and populated all files

---
+++ concatenate.py
@@ -0,0 +1,3 @@
+def add_string(s,t):
+
+    return s+t
---
+++ test.py
@@ -0,0 +1,32 @@
+import unittest
+
+from addition import add
+from concatenate import add_string
+
+class Test(unittest.TestCase):
+    def test_functions(self):
+
+        """
+        Test case to add two numbers
+        """
+       
+       x,y = 2,3
+       result1 = add(x,y)
+
+       self.assertEqual(result1,5)
+       self.assertEqual(result1,7)
+
+       #Test cases to check concatenation
+
+       s="Hi "
+       t = "Sooraj!"
+
+       result2 = add_string(s,t)
+
+       self.assertEqual(result2,"Wassup")
+       self.assertEqual(result2,"Hi Sooraj!")
+    
+    
+
+if __name__ == '__main__':
+    unittest.main()
